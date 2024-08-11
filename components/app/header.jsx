b0c39f499336e70e9093d7929371a746ce4da000
jaczerob <jacob@jaczerob.dev>
2024-08-11 16:47:55 -0400

    more headers

--- components/app/header.jsx
+++ components/app/header.jsx
@@ -2,8 +2,12 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import {Dialog, DialogContent, DialogHeader, DialogTitle} from "../ui/dialog";
+import {useState} from "react";
 
 export default function Header(props) {
+    const [openVote, setOpenVote] = useState(false);
+
     return (
         <header {...props}>
             <div className="container mx-auto flex flex-col items-center justify-center">
@@ -15,6 +19,22 @@ export default function Header(props) {
                     <h5 className="text-center text-white" style={{
                         textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
                     }}>DISCORD</h5>
+                </Link>
+                <Link href="#"
+                      className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
+                      prefetch={false}
+                      onClick={() => alert("Type @vote in game to vote!")}
+                >
+                    <h5 className="text-center text-white" style={{
+                        textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
+                    }}>VOTE</h5>
+                </Link>
+                <Link href="#" className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
+                      prefetch={false}
+                      onClick={() => alert("You can auto-register through the client!")}>
+                    <h5 className="text-center text-white" style={{
+                        textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
+                    }}>REGISTER</h5>
                 </Link>
             </nav>
         </header>
