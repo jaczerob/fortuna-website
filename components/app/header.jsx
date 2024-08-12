73a59cf0c2f5f37ca887ca8dd96ca1b2aeae243e
jaczerob <jacob@jaczerob.dev>
2024-08-12 16:00:52 -0400

    change link underline color

--- components/app/header.jsx
+++ components/app/header.jsx
@@ -14,8 +14,13 @@ export default function Header(props) {
                 <Image src="/logo.png" alt="Logo" width="1000" height="100"/>
             </div>
             <nav className="md:flex gap-4 mt-6 items-center justify-center h-16 grid-cols-2 grid">
-                <Link target="_blank" href="https://discord.gg/BWq9u2t8rq" className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
-                      prefetch={false}>
+                <Link
+                    target="_blank"
+                    href="https://discord.gg/BWq9u2t8rq"
+                    className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
+                    prefetch={false}
+                    style={{ textDecorationColor: `#E2BC44`, textDecorationThickness: `3px` }}
+                >
                     <h5 className="text-center text-white" style={{
                         textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
                     }}>DISCORD</h5>
@@ -24,6 +29,7 @@ export default function Header(props) {
                       className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
                       prefetch={false}
                       onClick={() => alert("Type @vote in game to vote!")}
+                      style={{ textDecorationColor: `#E2BC44`, textDecorationThickness: `3px` }}
                 >
                     <h5 className="text-center text-white" style={{
                         textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
@@ -31,7 +37,8 @@ export default function Header(props) {
                 </Link>
                 <Link href="#" className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
                       prefetch={false}
-                      onClick={() => alert("You can auto-register through the client!")}>
+                      onClick={() => alert("You can auto-register through the client!")}
+                      style={{ textDecorationColor: `#E2BC44`, textDecorationThickness: `3px` }}>
                     <h5 className="text-center text-white" style={{
                         textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
                     }}>REGISTER</h5>
