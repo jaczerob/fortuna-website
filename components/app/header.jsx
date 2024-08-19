b4dac6fcfcf089fce22d344a5f6fa0e2d1bc16af
jaczerob <jacob@jaczerob.dev>
2024-08-19 00:47:22 -0400

    make registration more clear

--- components/app/header.jsx
+++ components/app/header.jsx
@@ -7,10 +7,10 @@ import {useState} from "react";
 import {Button} from "../ui/button";
 import {Input} from "../ui/input";
 import RegisterUser from "../actions/register";
-import {useSession} from "next-auth/react";
+import {signIn, useSession} from "next-auth/react";
 
 export default function Header(props) {
-    const { data: session, status } = useSession();
+    const {data: session, status} = useSession();
     const id = session?.user?.id;
 
     return (
@@ -18,16 +18,20 @@ export default function Header(props) {
             <div className="container mx-auto flex flex-col items-center justify-center">
                 <Image src="/logo.png" alt="Logo" width="1000" height="100"/>
             </div>
-                {
-                    status === 'authenticated' ?
-                        <AuthenticatedLinks discordId={id} className="md:flex gap-4 mt-6 items-center justify-center h-16 grid-cols-2 grid"/> :
-                        <UnauthenticatedLinks className="md:flex gap-4 mt-6 items-center justify-center h-16 grid-cols-2 grid"/>
-                }
+            {
+                status === 'authenticated' ?
+                    <AuthenticatedLinks discordId={id}
+                                        className="md:flex gap-4 mt-6 items-center justify-center h-16 grid-cols-2 grid"/> :
+                    <UnauthenticatedLinks
+                        className="md:flex gap-4 mt-6 items-center justify-center h-16 grid-cols-2 grid"/>
+            }
         </header>
     )
 }
 
 function UnauthenticatedLinks(props) {
+    const [open, setOpen] = useState(false);
+
     return (
         <nav {...props}>
             <Link
@@ -41,16 +45,33 @@ function UnauthenticatedLinks(props) {
                     textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
                 }}>DISCORD</h5>
             </Link>
-            <Link
-                href="/api/auth/signin"
-                className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
-                prefetch={false}
-                style={{textDecorationColor: `#E2BC44`, textDecorationThickness: `3px`}}
+            <Link href="#"
+                  className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
+                  prefetch={false}
+                  onClick={() => alert("Type @vote in game to vote!")}
+                  style={{textDecorationColor: `#E2BC44`, textDecorationThickness: `3px`}}
             >
                 <h5 className="text-center text-white" style={{
                     textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
-                }}>SIGN-IN</h5>
+                }}>VOTE</h5>
             </Link>
+            <Link href="#" className="text-3xl font-medium hover:underline underline-offset-4 pr-4"
+                  prefetch={false}
+                  onClick={() => setOpen(true)}
+                  style={{textDecorationColor: `#E2BC44`, textDecorationThickness: `3px`}}
+            >
+                <h5 className="text-center text-white" style={{
+                    textShadow: `3px 0 #E2BC44, -3px 0 #E2BC44, 0 3px #E2BC44, 0 -3px #E2BC44, 3px 3px #E2BC44, -3px -3px #E2BC44, 3px -3px #E2BC44, -3px 3px #E2BC44`
+                }}>CREATE ACCOUNT</h5>
+            </Link>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent className="bg-white content-center text-center">
+                    <DialogDescription>
+                        To create an account, you first need to authenticate through Discord<br/><br/>
+                        <Button onClick={() => signIn()} className="rounded-lg bg-white hover:underline underline-offset-4 border-black mt-2">Authenticate</Button>
+                    </DialogDescription>
+                </DialogContent>
+            </Dialog>
         </nav>
     )
 }
@@ -94,16 +115,14 @@ function AuthenticatedLinks(props) {
             <Dialog open={open} onOpenChange={setOpen}>
                 <DialogContent className="bg-white content-center text-center">
                     <DialogDescription>
-                        <Input type="text" placeholder="Username" value={usernameStr} className="mt-2"
+                        <Input type="text" placeholder="Username" value={usernameStr} className="mt-4"
                                onChange={(ev) => setUsernameStr(ev.target.value)}/>
                         <Input type="password" placeholder="Password" className="mt-2 mb-2" value={passwordStr}
                                onChange={(ev) => setPasswordStr(ev.target.value)}/>
                         <Button className="rounded-lg bg-white border-black border-[1px]" onClick={() => {
                             RegisterUser(usernameStr, passwordStr, props.discordId)
                                 .then(response => alert(response))
-                                .catch(error => {
-                                    alert(error.message);
-                                })
+                                .catch(error => alert(error.message))
                                 .finally(() => setOpen(false));
                         }}>Register</Button>
                     </DialogDescription>
@@ -111,8 +130,4 @@ function AuthenticatedLinks(props) {
             </Dialog>
         </nav>
     )
-}
-
-function getDiscordID() {
-
 }
