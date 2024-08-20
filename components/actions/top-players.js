d02f087b0f3ae84dcc1634387fb9e4cf8d80a639
jaczerob <jacob@jaczerob.dev>
2024-08-20 17:01:54 -0400

    fix top players query

--- components/actions/top-players.js
+++ components/actions/top-players.js
@@ -4,13 +4,16 @@
 import prisma from "../../db";
 
 export default async function GetTopPlayers() {
-    return prisma.$queryRaw`SELECT cs.id, cs.name, cs.level, cs.job
-                                     FROM characterstats cs,
-                                          characters c,
-                                          users u
-                                     WHERE c.id = cs.id
-                                       AND c.accid = u.id
-                                       AND u.accounttype = 0
-                                       AND (u.banExpireDate IS null OR u.banExpireDate <= NOW())
-                                     ORDER BY cs.level DESC LIMIT 5;`;
+    return prisma.$queryRaw`SELECT cs.id, cs.name, cs.level, cs.job, u.accounttype, u.name
+                            FROM characterstats cs,
+                                 characters c,
+                                 users u,
+                                 accounts a
+                            WHERE c.id = cs.id
+                              AND c.accid = a.id
+                              AND a.userid = u.id
+                              AND u.accounttype = 0
+                              AND (u.banExpireDate IS null OR u.banExpireDate <= NOW())
+                            ORDER BY cs.level DESC
+                                LIMIT 5;`;
 }
